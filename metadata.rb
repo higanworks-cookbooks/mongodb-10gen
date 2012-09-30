7d84018ac27d738505bfcfa46ba1ebf92fcbdc69
sawanoboly <sawanoboriyu@higanworks.com>
2012-09-30 17:23:55 +0900

    depend apt

--- metadata.rb
+++ metadata.rb
@@ -3,4 +3,5 @@ maintainer_email "sawanoboriyu@higanworks.com"
 license          "MIT"
 description      "Installs/Configures mongodb-10gen"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.2"
+depends          "apt"
