4ebbdfe363c2b9799d615273d1170dd096406225
sawanoboly <sawanoboriyu@higanworks.com>
2012-10-23 12:04:24 +0900

    add sharding_importer

--- recipes/branch20_six.rb
+++ recipes/branch20_six.rb
@@ -6,19 +6,19 @@ node.load_attribute_by_short_filename("default","mongodb-10gen")
 
 (1..6).each do |idx|
 
-  directory File.join(node['mongodb']['data_dir'], "mongodb#{idx}") do
+  directory ::File.join(node['mongodb']['data_dir'], "mongodb#{idx}") do
     owner "mongodb"
     group "mongodb"
     mode 00700
   end
 
-  directory File.join(node['mongodb']['log_dir']) do
+  directory ::File.join(node['mongodb']['log_dir']) do
     owner "mongodb"
     group "mongodb"
     mode 00755
   end
 
-  template File.join("/etc/init", "mongodb#{idx}.conf") do
+  template ::File.join("/etc/init", "mongodb#{idx}.conf") do
     source "init_mongodb.erb"
     owner "root"
     group "root"
@@ -30,7 +30,7 @@ node.load_attribute_by_short_filename("default","mongodb-10gen")
     )
   end
 
-  template File.join("/etc/logrotate.d", "mongodb#{idx}") do
+  template ::File.join("/etc/logrotate.d", "mongodb#{idx}") do
     source "logrotate_mongodb.erb"
     owner "root"
     group "root"
@@ -41,7 +41,7 @@ node.load_attribute_by_short_filename("default","mongodb-10gen")
     )
   end
 
-  template File.join(node['mongodb']['etc_dir'],"mongodb#{idx}.conf") do
+  template ::File.join(node['mongodb']['etc_dir'],"mongodb#{idx}.conf") do
     source "mongodb.conf.erb"
     owner "mongodb"
     group "mongodb"
@@ -72,4 +72,23 @@ node.load_attribute_by_short_filename("default","mongodb-10gen")
     action [:enable, :start]
   end
 
+end
+
+
+# create config for sharding.
+
+shardurls = []
+(1..6).each do |idx|
+  port = node['mongodb']['baseport'] + 5000 * idx.to_i
+  shardurls.push "127.0.0.1:#{port.to_s}"
+end
+
+template ::File.join(node['mongodb']['misc_dir'], "sharding_import.config") do
+  source "sharding_import.config.erb"
+  owner "mongodb"
+  group "mongodb"
+  mode 00644
+  variables(
+    :shardurls => shardurls
+  )
 end
