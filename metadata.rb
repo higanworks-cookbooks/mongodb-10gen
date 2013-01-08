558eef0c7cf467e6d2969593a0d34470b3180476
sawanoboly <sawanoboriyu@higanworks.com>
2013-01-08 14:56:05 +0900

    bump v0.1.6

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email "sawanoboriyu@higanworks.com"
 license          "MIT"
 description      "Installs/Configures mongodb-10gen"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.5"
+version          "0.1.6"
 depends          "apt"
 depends          "mongodb-10gen" # workaround for TravisCI
